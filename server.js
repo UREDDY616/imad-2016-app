32f077e6bf5a9fe6640f791f722e509eb95509a5
UREDDY616 <201551081@iiitvadodara.ac.in>
2016-12-20 11:41:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,9 @@ app.get('/:articleName', function (req, res) {
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
+app.get('/nithisha', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'nithisha.html'));
+});
 app.get('/article-three', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
